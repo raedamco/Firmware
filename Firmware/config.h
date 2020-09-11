c4dcea903d132a5037980437e0eafb7187e54ab6
Omar <waked@icloud.com>
2020-09-10 18:14:31 -0700

    Firmware
    Repalce painlessmesh libraray with pure BLE
    - Missing the forward function/recieve functin from whatever data it recieves from the previous node

---
+++ Firmware/config.h
@@ -0,0 +1,21 @@
+//
+//  config.h
+//  Raedam
+//
+//  Created on 1/10/2019. Modified on 9/10/2020.
+//  Copyright © 2020 Raedam Inc. All rights reserved.
+//
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define UNIQUE_ID 1
+
+#define PIN_ECHO 27
+#define PIN_TRIG 12
+
+#define DEBUG
+
+#define COLLECT_TIME 1000 /* 900000 in milliseconds = 15 minutes*/
+
+#endif
---
+++ Firmware/ultrasonic.h
@@ -0,0 +1,34 @@
+//
+//  ultrasonic.h
+//  Raedam
+//
+//  Created on 1/10/2019. Modified on 9/10/2020.
+//  Copyright © 2020 Raedam Inc. All rights reserved.
+//
+
+#ifndef ULTRASONIC_H
+#define ULTRASONIC_H
+
+#define ULTRASONIC_CID 1
+
+class ultrasonic {
+  
+public:
+	ultrasonic(int trig, int echo);
+	int get();
+	float getCM();
+	float getIN();
+	bool isOccupied();
+  
+private:
+	void init();
+	const int _ECHO_DURATION = 10;
+	const float _ECHO_TO_CM = 58.2;
+	const float _ECHO_TO_IN = 144;
+	const int _IS_OCCUPIED_DISTANCE_CM = 100;
+
+	int _pinTrig;
+	int _pinEcho;
+};
+
+#endif
