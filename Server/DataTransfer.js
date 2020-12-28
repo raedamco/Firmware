60929bcc2bd15351d534299eab0a0bcb585d0517
Austin Mckee <captainlantern8@gmail.com>
2020-12-28 12:49:55 -0800

    doc if test

--- Server/DataTransfer.js
+++ Server/DataTransfer.js
@@ -111,7 +111,7 @@ async function doc_grab (data_path)
           temp = [String(temp)];
           console.log(temp);
           await console.log(doc.data().temp);
-          console.log(doc.data().Organization);
+          console.log(doc.data().[ 'Pricing' ]);
           //console.log(doc.data()."Spot Types");
 //            (snapshot => {
 //                snapshot.docs.forEach(doc =>{
