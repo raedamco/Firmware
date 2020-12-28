903299b97ebd6a7e1cb4f5e2478378656314245c
Austin Mckee <captainlantern8@gmail.com>
2020-12-28 12:30:29 -0800

    doc if test

--- Server/DataTransfer.js
+++ Server/DataTransfer.js
@@ -108,6 +108,7 @@ async function doc_grab (data_path)
           let fields = Object.keys(doc.data());
           console.log(fields);
           let temp = await fields[0];
+          console.log(temp)
           await console.log(doc.data().temp);
           //console.log(doc.data()."Spot Types");
 //            (snapshot => {
