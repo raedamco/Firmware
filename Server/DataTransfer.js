5fb4fe2a772ada7ce3b504c9861142d4a7d56636
Austin Mckee <captainlantern8@gmail.com>
2020-12-28 20:00:30 -0800

    recursions start

--- Server/DataTransfer.js
+++ Server/DataTransfer.js
@@ -40,6 +40,7 @@ function move_data(dest,src)
 // function copies desination including all sub-documents and stores in data structure that it returns 
 async function grab_data(src)
 {
+    console.log(src);
     let doc = false;
     let collection = false;
      //determine if src is a doc or collection      
