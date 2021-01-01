52baa0cc25e3d1bbffffe20856fe01214af288ac
Austin Mckee <captainlantern8@gmail.com>
2020-12-31 16:47:43 -0800

    root test

--- Server/DataTransfer.js
+++ Server/DataTransfer.js
@@ -112,7 +112,7 @@ async function grab_data(src)
 //             root.subDoc.push(sub);
 //          });
              // return object created
-                  console.log("Root:::::::::::::::"+ root.subDoc);
+                  console.log("Root:::::::::::::::"+ root.subDoc.id);
             return root;
        
         }
