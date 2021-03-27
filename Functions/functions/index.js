cd76b7367d900419e2f38a054b057e410e16a980
Austin Mckee <captainlantern8@gmail.com>
2021-03-27 14:45:24 -0700

    functional in theory

--- Functions/functions/index.js
+++ Functions/functions/index.js
@@ -3,6 +3,7 @@ const functions = require("firebase-functions");
 
 // The Firebase Admin SDK to access Firestore.
 const admin = require("firebase-admin");
+const { user } = require("firebase-functions/lib/providers/auth");
 admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
@@ -16,9 +17,29 @@ admin.initializeApp();
 exports.batteryLevelNotify = functions.firestore
   .document(`Companies/{companyName}/Data/{structure}`)
   .onUpdate((snap, context) => {
-    const mainUnits = snap.data()["Main Units"]; // array of units in floor
-    functions.logger.log(
-      `Battery Level for ${context.params.structure} is ${mainUnits["Unit 1"]["Battery Level"]}%`
-    );
+    const db = admin.firestore();
+    const mainUnits = snap.after.data()["Main Units"]; // array of units in floor
+    const users = db.collection(`Users`).doc(`Companies`).collection(`Users`);
+    updateUsers(users, context, mainUnits);
+
     return null; //snap.ref.set({mainUnits}, {merge: false});
   });
+
+async function updateUsers(users, context, mainUnits) {
+  const userMatches = await users
+    .where("Companies", `array-contains`, `${context.params.companyName}`)
+    .get();
+  if (!userMatches.empty) {
+    userMatches.forEach((doc) => {
+      const theMessages = doc.data().messages;
+      theMessages.push(
+        `Battery Level for ${context.params.structure} is ${mainUnits["Unit 1"]["Battery Level"]}%`
+      );
+      users.doc(`${doc.id}`).update({
+        messages: theMessages,
+      });
+    });
+  } else {
+    functions.logger.log("NO matching Users for Unit");
+  }
+}
