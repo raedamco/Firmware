cbe9c5f0b381d68f2446907dab98c7d72ebd55d6
Austin Mckee <captainlantern8@gmail.com>
2021-04-25 15:48:30 -0700

    pre-break

--- Scripts/BusinessUsers/users.controller.js
+++ Scripts/BusinessUsers/users.controller.js
@@ -1,6 +1,13 @@
 const service = require("./users.service");
 
-async function create(req, res) {}
+async function create(req, res, next) {
+  if (req.body.data) {
+    const newUser = req.body.data;
+    const data = await service.create(newUser);
+    return res.json({ data });
+  }
+  // next({ status: 400, message: `Missing update data` });
+}
 
 module.exports = {
   create,
